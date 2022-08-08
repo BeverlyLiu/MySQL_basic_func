3c7083865169a10e90d53a3606aafba5127c8cd4
BeverlyLiu <bevliu@gmail.com>
2022-08-07 17:05:26 -0700

    The Limit Clause

---
+++ LIMIT.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM customers
+ORDER BY state, first_name DESC;
+
+SELECT first_name, last_name
+FROM customers
+ORDER BY birth_date;
+
+SELECT first_name, last_name, 10 AS points
+FROM customers
+ORDER BY points, first_name;
+
+SELECT first_name, last_name, 10 AS points
+FROM customers
+ORDER BY first_name;
+
+SELECT *, quantity * unit_price AS total_price
+FROM order_items
+WHERE order_id = 2
+ORDER BY total_price DESC
